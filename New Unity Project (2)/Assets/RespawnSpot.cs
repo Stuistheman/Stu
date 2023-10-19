29fc6d83f1425ee317ea795fec29ed8b8c51c5aa
Stuart Philip <S290526@cfisd.loc>
2023-10-19 12:41:08 -0500

    New respawns working

--- New Unity Project (2)/Assets/RespawnSpot.cs
+++ New Unity Project (2)/Assets/RespawnSpot.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class RespawnSpot : MonoBehaviour
 {
 	public float threshold;
+    public Vector3 repos; 
+
+    public int relocation; 
     // Update is called once per frame
+void Start(){
+    repos = new Vector3(-0.26f, 2.159f, 2.72f);
+}
     void Update()
     {
         if(transform.position.y < threshold){
-	transform.position = new Vector3(-0.26f, 2.159f, -2.72f);
+	transform.position = repos;
 }
+
+
+    }
+    void OnCollisionEnter(Collision theCollision){
+
+        if(theCollision.gameObject.tag == "Respawn"){
+            repos = theCollision.gameObject.transform.position;
+        }
     }
 }
