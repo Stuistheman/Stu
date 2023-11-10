03121d85bbc2d5eb0f57ea7832198ee1601b1b17
Stuart Philip <S290526@cfisd.loc>
2023-11-10 12:44:49 -0600

    Work on moving platform sticking YOU CAN DO THIS WOO

--- New Unity Project (2)/Assets/PlatformDetection.cs
+++ New Unity Project (2)/Assets/PlatformDetection.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {   
     public GameObject player;
-
+    private GameObject target;
+    private Vector3 offset;
 
     // Start is called before the first frame update
 
 
+    void Start(){
+        target = null;
+    }
     // Update is called once per frame
-    private void OnTriggerEnter(Collider other){
-        if(other.gameObject == player){
-            player.transform.parent = transform; 
-        }
+    private void OnTriggerStay(Collider other){
+        target = other.gameObject;
+	     offset = target.transform.position - transform.position;
+
     }
 
     private void OnTriggerExit(Collider other){
-        if(other.gameObject == player){
-            player.transform.parent = null;
-        }
+        target = null;
     }
+    void LateUpdate(){
+	if (target != null) {
+		target.transform.position = transform.position+offset;
+	}
+}
 }
